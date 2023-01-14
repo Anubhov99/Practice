947330ef1e651eb54ae8aa02ea4db92ca34081be
Anubhov99 <m.anubhov99@gmail.com>
2023-01-14 12:42:14 +0530

    Added age.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter your name :");
+        System.out.println("Enter your name & age :");
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
+        int age = sc.nextInt();
         System.out.println("Your name is "+s);
+        System.out.println("Your age is "+age);
     }
 }
